088dbaefc1536bf8e7a99c2f6473a9011f18b31d
PARAVZIL <93379770+PARAVZIL@users.noreply.github.com>
2022-03-15 13:13:41 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAUlv6k3BWxnpqsmTj9UtANDHe-AQ0zkHQ",
+    authDomain: "quitter-f4e68.firebaseapp.com",
+    databaseURL: "https://quitter-f4e68-default-rtdb.firebaseio.com",
+    projectId: "quitter-f4e68",
+    storageBucket: "quitter-f4e68.appspot.com",
+    messagingSenderId: "438447825861",
+    appId: "1:438447825861:web:12a4a975820e9c58bee1ac"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
